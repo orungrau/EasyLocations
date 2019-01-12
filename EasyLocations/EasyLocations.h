d9c0c7b8c9a4fc55ed5e9c6a4fdeea6ff20df2c4
Anatoly Myaskov <myaskov@me.com>
2019-01-12 14:49:48 +0300

    Initial Commit

---
+++ EasyLocations/EasyLocations.h
@@ -0,0 +1,19 @@
+//
+//  EasyLocations.h
+//  EasyLocations
+//
+//  Created by Anatoly Myaskov on 1/12/19.
+//  Copyright © 2019 Anatoly Myaskov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyLocations.
+FOUNDATION_EXPORT double EasyLocationsVersionNumber;
+
+//! Project version string for EasyLocations.
+FOUNDATION_EXPORT const unsigned char EasyLocationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyLocations/PublicHeader.h>
+
+
